4e42d42415d2f05a50692e5ba044bcd2a4ae8af3
Amit Nadir <Sangeet@sangeets-macbook-pro.local>
2016-12-29 00:32:32 +0200

    First commit

---
+++ tweetsEngine/Tweet.swift
@@ -0,0 +1,13 @@
+//
+//  Tweet.swift
+//  tweetsEngine
+//
+//  Created by Amit on 29/12/2016.
+//  Copyright © 2016 mobi-Wiz. All rights reserved.
+//
+
+import UIKit
+
+class Tweet: NSObject {
+
+}
