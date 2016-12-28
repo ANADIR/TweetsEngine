c95b6d33f414ed2b5e79d1b116df8fd071d751a4
Amit Nadir <Sangeet@sangeets-macbook-pro.local>
2016-12-29 00:26:53 +0200

    Initial Commit

---
+++ tweetsEngine/tweetsEngine.h
@@ -0,0 +1,19 @@
+//
+//  tweetsEngine.h
+//  tweetsEngine
+//
+//  Created by Amit on 29/12/2016.
+//  Copyright © 2016 mobi-Wiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for tweetsEngine.
+FOUNDATION_EXPORT double tweetsEngineVersionNumber;
+
+//! Project version string for tweetsEngine.
+FOUNDATION_EXPORT const unsigned char tweetsEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <tweetsEngine/PublicHeader.h>
+
+
